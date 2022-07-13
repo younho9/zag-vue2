a2f683e7bd5a758e0d123ccdc5e575873409bc70
Younho Choo <yo+github@younho9.com>
2022-07-13 13:26:42 +0900

    feat: add VAccordion

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue';
+import VAccordion from './components/VAccordion.vue';
 </script>
 
 <template>
 	<div id="app">
 		<img alt="Vue logo" src="./assets/logo.png" />
-		<HelloWorld msg="Hello Vue 2 + TypeScript + Vite" />
+		<VAccordion />
 	</div>
 </template>
 
